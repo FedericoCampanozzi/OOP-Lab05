b2b0b70ad3c942034c4ea4746d698987157f89f4
federico.campanozzi@gmail.com <federico.campanozzi@studio.unibo.it>
2020-11-11 17:57:23 +0100

    Fintio es 5

--- src/it/unibo/oop/lab05/ex5/Utilities.java
+++ src/it/unibo/oop/lab05/ex5/Utilities.java
@@ -1,6 +1,7 @@
 package it.unibo.oop.lab05.ex5;
 
 import java.util.Collection;
+import java.util.Random;
 
 /**
  *
@@ -21,6 +22,9 @@ public final class Utilities {
      *            Collection type
      */
     public static <X> void copyAll(final Collection<X> source, final Collection<? super X> target) {
+        for (final X elem : source) {
+            target.add(elem);
+        }
     }
 
     /**
@@ -32,6 +36,13 @@ public final class Utilities {
      * 
      */
     public static <X> X getRandomElement(final Collection<X> coll) {
+        int n = new Random().nextInt(coll.size());
+        for (final X elem : coll) {
+            if (n == 0) {
+                return elem;
+            }
+            n--;
+        }
         return null;
     }
 
@@ -47,6 +58,6 @@ public final class Utilities {
      * @return a pair with two random elements
      */
     public static <X, Y> Pair<X, Y> getRandomPair(final Collection<X> one, final Collection<Y> two) {
-        return null;
+    	return new Pair<>(getRandomElement(one), getRandomElement(two));
     }
 }
